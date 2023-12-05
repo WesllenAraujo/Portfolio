6b1ae17e7fe34b39799f7ef505a9656037a2b3e7
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-12-05 15:35:19 -0300

    Finished the project

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+window.addEventListener('wheel', function(e) {
+    e.preventDefault();
+}, { passive: false });
+let menuItemList = document.querySelectorAll(".menu-item");
+menuItemList.forEach((button, i) => {
+    button.addEventListener("click", function jumpCarroselPage() {
+        console.log(button + " " + i);
+        let slideList = document.querySelectorAll(".slide");
+        switch (i) {
+            case 0:
+                for (let i2 = 0; i2 < slideList.length; i2++) {
+                    slideList[i].style.marginLeft = "0";
+                }
+                break;
+                case 1:
+                    slideList[0].style.marginLeft = "-101%";
+                    break;
+                case 2:
+                    slideList[0].style.marginLeft = "-202%";
+                    break;
+                case 3:
+                    slideList[0].style.marginLeft = "-303%";
+                    break;
+            default:
+                slideList[0].style.marginLeft = "0";
+                break;
+        }
+    })
+})
